4de51401064cdd791a36bc130c9f7edd065b07d8
Hack Reactor Students <jackson.clowes@gmail.com>
2013-11-13 21:38:49 -0800

    Added collab files

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,24 +1,39 @@
 // Note: don't use an array to do this.
 var makeLinkedList = function(){
   var list = {};
+  var storage = {};
   list.head = null;
   list.tail = null;
+  list.order = 0;
+  list.lastRemoved = -1;
+  list.size = 0;
+  var containsObject = {};
 
-  list.addToTail = function(){
+  list.addToTail = function(num){
+    storage[list.order] = makeNode(num);
+    containsObject[storage[list.order.value]] = true;
+    storage[list.order-1].value = storage[list.order];
+    list.order++;
+    list.size++;
   };
 
   list.removeHead = function(){
-  };
+    if(list.size) {
+      list.size--;
+      list.lastRemoved++;
+      return list[lastRemoved];
+    }};
 
-  list.contains = function(){
+  list.contains = function(num){
+    _.contains(Object.keys(containsObject), num);
   };
 
   return list;
 };
 
-var makeNode = function(value){
+var makeNode = function(num){
   var node = {};
-  node.value = value;
+  node.value = num;
   node.next = null;
 
   return node;
